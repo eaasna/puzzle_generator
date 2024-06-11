40156134a78427796da6bdd7fcf2531e5ebeb5dc
Evelin <evelin.aasna@fu-berlin.de>
2024-06-11 15:36:46 +0200

    Hello piston 2d graphics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+extern crate piston_window;
+
+use piston_window::*;
+
 fn main() {
     use markov::Chain;
     let mut chain = Chain::of_order(4);
@@ -33,5 +37,17 @@ fn main() {
             println!("");
             n += 1;
         }
+    }
+
+    let mut window: PistonWindow =
+        WindowSettings::new("Hello Piston!", [640, 480])
+        .exit_on_esc(true).build().unwrap();
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |c, g, _device| {
+            clear([1.0; 4], g);
+            rectangle([1.0, 0.0, 0.0, 1.0], // red
+                      [0.0, 0.0, 100.0, 100.0],
+                      c.transform, g);
+        });
     }
 }
