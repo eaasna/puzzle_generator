13e4119acaf7dab6e0edd646c2c5eadaeb834b16
Evelin <evelin.aasna@fu-berlin.de>
2025-06-26 17:08:06 +0200

    Try to make a checker board pattern

--- src/miniquad.rs
+++ src/miniquad.rs
@@ -32,16 +32,24 @@ impl Stage {
                     color: [0., 0., 1., 0.]
                 }
             } else {
-            Vertex {
-                pos : [-1. + 2.*(((i-N*N)%N) as f32)/(N as f32 -1.),  // x
-                       -1. + 2.*(((i-N*N)/N) as f32)/(N as f32 -1.) ], // y
-                color: [1., 0., 0., 1.]
-            }
+                Vertex {
+                    pos : [-1. + 2.*(((i-N*N)%N) as f32)/(N as f32 -1.),  // x
+                           -1. + 2.*(((i-N*N)/N) as f32)/(N as f32 -1.)], // y
+                    color: [1., 0., 0., 1.]
+                }
         }
         });
 
-        for i in 0..2*N*N {
-            print!("Vertex {},{}\n", vertices[i].pos[0], vertices[i].pos[1]);
+        print!("BLUE\n");
+
+        for i in 0..N*N {
+            print!("Vertex {}: {},{}\n", i, vertices[i].pos[0], vertices[i].pos[1]);
+        }
+
+        print!("RED\n");
+
+        for i in N*N..2*N*N {
+            print!("Vertex {}: {},{}\n", i, vertices[i].pos[0], vertices[i].pos[1]);
         }
         
         let vertex_buffer = ctx.new_buffer(
@@ -61,29 +69,33 @@ impl Stage {
             indices[i] = (s[i % (N+1)] + i/(3*2) + add) as u16;
         }
 
-        add = 0;
-        for i in T..(T+1)+((T as f32)/2.0) as usize {
-            let ind = i - T + 1;
-            if ind % (N-1) > 2 {
-                add += 6;
-            }
-            if (ind % ((N-1)*3*2) == 0) && (ind != 0) {
-                add += 1;
-            }
-            indices[i] = (s[ind % (N+1)] + i/(3*2) + add) as u16;
+        print!("BLUE\n");
+
+        for i in (0..T).step_by(3) {
+            print!("Index {}: {},{},{}\n", i, indices[i], indices[i+1], indices[i+2]);
         }
 
-        for i in (0..T+1).step_by(3) {
-            print!("Index {},{},{}\n", indices[i], indices[i+1], indices[i+2]);
+        add = 0;
+        for i in 0..T as usize {
+            /*if i % 12 < 6 {
+                add += 1;
+            }*/
+            if (i % ((N-1)*3*2) == 0) && (i != 0) {
+                add += 1;
+            }
+            indices[i+T] = (s[i % (N+1)] + i/(3*2) + N*N + add) as u16;
+            if (i % 12 < 6) && (add % 2 == 0) {
+                indices[i+T] += 1;
+            }
         }
 
         print!("RED\n");
 
-        for i in (T+1..T*2-2).step_by(3) {
-            print!("Index {},{},{}\n", indices[i], indices[i+1], indices[i+2]);
+        for i in (T..T*2-2).step_by(3) {
+            print!("Index {}: {},{},{}\n", i, indices[i], indices[i+1], indices[i+2]);
         }
 
-        let index_buffer1 = ctx.new_buffer(
+        let index_buffer = ctx.new_buffer(
             BufferType::IndexBuffer,
             BufferUsage::Immutable,
             BufferSource::slice(&indices),
@@ -91,7 +103,7 @@ impl Stage {
 
         let bindings = Bindings {
             vertex_buffers: vec![vertex_buffer],
-            index_buffer: index_buffer1,
+            index_buffer: index_buffer,
             images: vec![],
         };
 
@@ -139,7 +151,7 @@ impl EventHandler for Stage {
         self.ctx.apply_bindings(&self.bindings);
 
         //for i in 0..(T as f32/6 as f32) as usize {
-        for i in 0..1 as usize {
+        for i in 0..2 as usize {
             let t = t + i as f64 * 0.3;
 
             self.ctx
